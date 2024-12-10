1f46ea5aabab1d0533a3e6f25801f509f980d85c
carosandovalcab <clsandov@umn.edu>
2024-12-10 10:42:37 -0600

    updated script to write FI and BW

--- scripts/data_proc.R
+++ scripts/data_proc.R
@@ -19,23 +19,31 @@ open_files <- filter_files %>%
   map_dfr(
     ., function(X){
       read_csv(X$filepath) %>% 
-        select(ID_MICE, FOOD_WEIGHT_START_G, FOOD_WEIGHT_END_G, DATE, DIET, BODY_WEIGHT_G) %>% 
+        select(ID, FOOD_WEIGHT_START_G, FOOD_WEIGHT_END_G, DATE, DIET, BODY_WEIGHT_G, DIET_FORMULA) %>% 
         mutate(
           INTAKE_GR = (FOOD_WEIGHT_START_G - FOOD_WEIGHT_END_G),
           DATE = lubridate::mdy(DATE)
         ) %>% 
-        select(ID_MICE, INTAKE_GR, DATE, BODY_WEIGHT_G) %>% 
+        select(ID, INTAKE_GR, DATE, BODY_WEIGHT_G, DIET_FORMULA) %>% 
         rename(
-          ID = ID_MICE,
           BW = BODY_WEIGHT_G
         )
     }
   )
 
+# load food description
+
+food_desc <- read_csv(paste(fname, "/food_description.csv", sep = ""))
+
+# load metadata
+
+metadata <- read_csv(paste(fname, "/META.csv", sep = "")) %>% 
+    select(ID, SEX, COHORT, STRAIN, AIM)
+
 # output food-intake file
 
 FI <- open_files %>% 
-  select(ID, INTAKE_GR, DATE) %>% 
+  select(ID, DIET_FORMULA, INTAKE_GR, DATE) %>% 
   group_by(ID) %>% 
   arrange(DATE, .by_group = TRUE) %>% 
   mutate(
@@ -44,14 +52,20 @@ FI <- open_files %>%
   drop_na(delta_measurement) %>% 
   mutate(
     corrected_intake_gr = INTAKE_GR / as.numeric(delta_measurement)
-  )
+  ) %>% 
+    left_join(., food_desc, by = "DIET_FORMULA") %>% 
+    mutate(
+        corrected_intake_kcal = corrected_intake_gr * KCAL_G
+    ) %>% 
+    left_join(., metadata, by = "ID")
 
 # output bodyweight file
 BW <- open_files %>% 
   group_by(ID) %>% 
   arrange(DATE, .by_group = TRUE) %>% 
   select(ID, BW, DATE) %>% 
-  drop_na(BW)
+  drop_na(BW) %>% 
+    left_join(., metadata, by = "ID")
 
-write_csv(x = FI, "../data/eegap/FI.csv")
-write_csv(x = BW, "../data/eegap/BW.csv")
+write_csv(x = FI, "../data/FI.csv")
+write_csv(x = BW, "../data/BW.csv")
