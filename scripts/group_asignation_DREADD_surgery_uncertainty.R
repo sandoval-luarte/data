da8df25e0bb7004c2704d94341146973387bfb10
carosandovalcab <clsandov@umn.edu>
2025-04-19 17:07:00 -0500

    COHORT 11 updates

--- scripts/group_asignation_DREADD_surgery_uncertainty.R
+++ scripts/group_asignation_DREADD_surgery_uncertainty.R
@@ -5,11 +5,13 @@
 #n=4 Males orexin - cre with control DREADD - uncertainty
 #n=4 Males orexin - cre with control DREAD - no uncertainty
 
+#libraries####
 library(ggplot2)
 library(readr)
 library(dplyr)
 library(tidyr)
-
+library(ggrepel)
+#Data import####
 echomri_data <-read_csv("~/Documents/GitHub/data/data/echomri.csv") #data import
 
 echomri_data <- echomri_data %>% 
@@ -18,13 +20,13 @@ echomri_data <- echomri_data %>%
   filter(SEX=="M") %>% 
   filter(ID != c(298, 315)) %>%  # died in surgery
   mutate(GROUP = case_when(
-    ID %in% c(314, 319, 321) ~ "OREXIN_CRE_CONTROL_NO_UNCERT",
-    ID %in% c(297, 318, 320) ~ "OREXIN_CRE_DREADD_UNCERT",
-    ID %in% c(308, 306, 305, 322) ~ "WT_CONTROL_NO_UNCERT",
-    ID %in% c(307, 316, 323, 325) ~ "OREXIN_CRE_CONTROL_UNCERT"
+    ID %in% c(321, 323, 325) ~ "OREXIN_CRE_CONTROL_NO_UNCERT",
+    ID %in% c(297, 313, 318, 320) ~ "OREXIN_CRE_DREADD_UNCERT",
+    ID %in% c(305, 306, 314, 322) ~ "WT_CONTROL_NO_UNCERT",
+    ID %in% c(307, 316, 319) ~ "OREXIN_CRE_CONTROL_UNCERT"
     )) %>% 
   drop_na()
-# Summarize the data
+# Summarize the data####
 summary_data <- echomri_data %>%
   group_by(GROUP) %>%
   summarise(
@@ -33,7 +35,7 @@ summary_data <- echomri_data %>%
     .groups = 'drop'
   )
 
-# Create the plot
+#plot####
 plot <- ggplot() +
   geom_point(data = echomri_data, aes(x = GROUP, y = adiposity_index), alpha = 0.7) +  # Optional: raw data points
   geom_point(data = summary_data, aes(x = GROUP, y = mean_adiposity), 
@@ -46,8 +48,43 @@ plot <- ggplot() +
   ylab("Adiposity Index") +
   xlab("Virus Group")
 plot
-# Check if adiposity index differs between groups using ANOVA
+# Check if adiposity index differs between groups using ANOVA####
 model <- lm(adiposity_index ~ GROUP, data = echomri_data)
 anova_result <- anova(model)
 print(anova_result) #no diff among groups 
+
+#postoperative period####
+bw <-read_csv("~/Documents/GitHub/data/data/BW.csv") #data import
+bw <- bw %>% 
+  filter(COHORT ==11) %>% 
+  select(ID,BW, DATE,STRAIN,COMMENTS) %>% #good so here we checked there is 27 animals in total (cohort 11)
+  filter(ID != c(298, 315)) %>%  # died in surgery
+  mutate(GROUP = case_when(
+    ID %in% c(321, 323, 325) ~ "OREXIN_CRE_CONTROL_NO_UNCERT",
+    ID %in% c(297, 313, 318, 320) ~ "OREXIN_CRE_DREADD_UNCERT",
+    ID %in% c(305, 306, 314, 322) ~ "WT_CONTROL_NO_UNCERT",
+    ID %in% c(307, 316, 319) ~ "OREXIN_CRE_CONTROL_UNCERT"
+  )) %>% 
+  filter(!is.na(GROUP))
+
+#plot####
+# Create a surgery_events dataframe
+surgery_events <- bw %>%
+  filter(COMMENTS %in% c("BILATERAL_CONTROL_DREADD_INJECTION_SURGERY", 
+                         "BILATERAL_INH_DREADD_INJECTION_SURGERY"))
+
+# Then, update your plot with an additional geom_point layer to mark those dates
+bw %>%
+  ggplot(aes(x = DATE, y = BW)) + 
+  geom_point(alpha = 0.2) +
+  geom_line(aes(group = ID)) +
+  geom_text(aes(label = ID), size = 2) +
+  geom_point(data = surgery_events, aes(x = DATE, y = BW), 
+             color = "red", size = 3, shape = 21, fill = "yellow") +  # Highlight the day of the surgery
+  labs(
+    x = "Date",
+    y = "body weight (g)"
+  ) +
+  facet_wrap(~STRAIN) +
+  theme_classic()
 
