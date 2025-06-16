bb46b76074e4a9ff999568b58c663e1a021f8c24
nicolasluarte <64244503+nicolasluarte@users.noreply.github.com>
2025-06-16 19:26:01 -0400

    updayr

--- FED3/entropy_computations.R
+++ FED3/entropy_computations.R
@@ -3,19 +3,20 @@ pacman::p_load(
     ggplot2,
     clipr,
     bigsnpr,
-    distributions3
+    distributions3,
+    ggthemes
 )
 
 # compute the entropy of a beta distribution
-differential_entropy <- function(alpha, beta){
-        tryCatch(-integrate(function(x) { 
-            dbeta(x, alpha, beta) * log(dbeta(x, alpha, beta)) 
-        }, lower = 0, upper = 1)$value, error=function(err) NA)
+differential_entropy <- function(alpha, beta) {
+    tryCatch(-integrate(function(x) {
+        dbeta(x, alpha, beta) * log(dbeta(x, alpha, beta))
+    }, lower = 0, upper = 1)$value, error = function(err) NA)
 }
 
 # sample from a set of delays to compute the total amount of time to get 150 pellets
 # given 15 sec, 2250 seconds per day / 0.83 hours is what im aiming for
-get_time <- function(delay_vector, probability_vector){
+get_time <- function(delay_vector, probability_vector) {
     sample(x = delay_vector, size = 1000, prob = probability_vector)
 }
 
@@ -27,10 +28,10 @@ beta_params <- expand_grid(
 beta_params
 
 # compute entropy and time for all the distributions
-beta_entropy <- beta_params %>% 
-    group_split(row_number()) %>% 
-    map_dfr(., function(X){
-        expected_delay <- (X$shape1 / (X$shape1+X$shape2))*150*150
+beta_entropy <- beta_params %>%
+    group_split(row_number()) %>%
+    map_dfr(., function(X) {
+        expected_delay <- (X$shape1 / (X$shape1 + X$shape2)) * 150 * 150
         expected_entropy <- differential_entropy(X$shape1, X$shape2)
         return(tibble(
             alpha = X$shape1,
@@ -41,17 +42,50 @@ beta_entropy <- beta_params %>%
     })
 
 # get those with an expected delay of 3000
-sub_beta_entropy <- beta_entropy %>% 
-    filter(expected_delay<3000, expected_delay>2000) %>% 
-    slice_max(order_by = expected_entropy, n=1)
+sub_beta_entropy <- beta_entropy %>%
+    filter(expected_delay < 3000, expected_delay > 2000) %>%
+    slice_max(order_by = expected_entropy, n = 1)
 sub_beta_entropy
 
-rbeta(shape1 = sub_beta_entropy$alpha, shape2 = sub_beta_entropy$beta,
-      n=1000) %>%  scales::rescale(., to = c(0,150)) %>% 
+r <- rbeta(
+    shape1 = sub_beta_entropy$alpha, shape2 = sub_beta_entropy$beta,
+    n = 1000
+) %>%
+    scales::rescale(., to = c(0, 150)) %>%
     trunc()
 
-table((rbeta(shape1 = sub_beta_entropy$alpha, shape2 = sub_beta_entropy$beta,
-           n=150)) %>% 
-    scales::rescale(., to = c(0,150)) %>% 
-        cut(., breaks=seq(0,150,15)))
+beta_plot <- 1:100 %>%
+    imap_dfr(., function(X, idx) {
+        r <- rbeta(
+            shape1 = sub_beta_entropy$alpha, shape2 = sub_beta_entropy$beta,
+            n = 1000
+        ) %>%
+            scales::rescale(., to = c(0, 150)) %>%
+            trunc()
+        return(tibble(delays = r) %>% mutate(run = idx))
+    })
 
+p1 <- beta_plot %>%
+    filter(run == 1) %>%
+    ggplot(aes(
+        delays,
+        color = run, group = run
+    )) +
+    geom_density(color = "black") +
+    theme_par() +
+    theme(
+        legend.position = "none"
+    ) +
+    ylab("Density") +
+    xlab("Delay in sec.")
+p1
+
+
+t <- table((rbeta(
+    shape1 = sub_beta_entropy$alpha, shape2 = sub_beta_entropy$beta,
+    n = 150
+)) %>%
+    scales::rescale(., to = c(0, 150)) %>%
+    cut(., breaks = seq(0, 150, 15)))
+
+tibble()
