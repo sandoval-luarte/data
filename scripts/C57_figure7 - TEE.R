f87b3e99f46b36f8bf99cfe581dfefa942561e4a
carosandovalcab <clsandov@umn.edu>
2025-09-26 16:43:35 -0500

    SCRIPTS C57 AND NZO TEE

--- scripts/C57_figure7 - TEE.R
+++ scripts/C57_figure7 - TEE.R
@@ -1,4 +1,4 @@
-#we aim to evaluate changes in 24h TEE in middle age NZO after different stages of feeding:
+#we aim to evaluate changes in 24h TEE in middle age C57 after different stages of feeding:
 #1 from baseline to peak obesity,
 #2:from peak of obesity to acute body weight loss
 #3 from acute body weight loss to body weight maintenance
@@ -22,33 +22,26 @@ sable_dwn <- readRDS(file = "../data/sable_downsampled_data.rds")
 
 #TEE####
 sable_TEE_data <- sable_dwn %>% # Load the data
-  filter(COHORT %in% c(3, 4, 5)) %>%   #we only want NZO mice
+  filter(COHORT ==2) %>%   #we only want C57 mice
   mutate(lights  = if_else(hr %in% c(20,21,22,23,0,1,2,3,4,5), "off", "on")) %>% 
   mutate(SABLE= case_when(
     sable_idx %in% c("SABLE_DAY_1",
                      "SABLE_DAY_2",
                      "SABLE_DAY_3",
-                     "SABLE_DAY_4",
-                     "SABLE_DAY_5",
+                     "SABLE_DAY_4") ~ "peak obesity",
+    sable_idx %in% c("SABLE_DAY_5",
                      "SABLE_DAY_6",
-                     "SABLE_DAY_7") ~ "baseline",
-    sable_idx %in% c("SABLE_DAY_8",
-                     "SABLE_DAY_9",
+                     "SABLE_DAY_7",
+                     "SABLE_DAY_8") ~ "BW loss",
+    sable_idx %in% c("SABLE_DAY_9",
                      "SABLE_DAY_10",
-                     "SABLE_DAY_11") ~ "peak obesity",
-    sable_idx %in% c("SABLE_DAY_12",
-                     "SABLE_DAY_13",
+                     "SABLE_DAY_11",
+                     "SABLE_DAY_12") ~ "BW maintenance", 
+    sable_idx %in% c("SABLE_DAY_13",
                      "SABLE_DAY_14",
-                     "SABLE_DAY_15") ~ "BW loss", 
-    sable_idx %in% c("SABLE_DAY_16",
-                     "SABLE_DAY_17",
-                     "SABLE_DAY_18",
-                     "SABLE_DAY_19") ~ "BW maintenance",
-    sable_idx %in% c("SABLE_DAY_20",
-                     "SABLE_DAY_21",
-                     "SABLE_DAY_22",
-                     "SABLE_DAY_23") ~ "BW regain",
-    )) %>% 
+                     "SABLE_DAY_15",
+                     "SABLE_DAY_16") ~ "BW regain"
+  )) %>% 
   filter(grepl("kcal_hr_*", parameter)) %>% # just to see TEE in kcal first
   ungroup() %>% 
   group_by(ID, SABLE) %>% 
@@ -63,25 +56,27 @@ sable_TEE_data <- sable_dwn %>% # Load the data
   ungroup() %>% 
   group_by(ID,complete_days,is_complete_day,SABLE) %>% 
   mutate(tee = sum(value)*(1/60)) %>% 
-  filter(!ID %in% c(3715,3712), is_complete_day ==1, complete_days==2) %>% #3715 died and 3723 has issues with the sable ,3723,3718,3709,3725
+  filter( is_complete_day ==1, complete_days==2) %>% 
   ungroup() %>% 
   group_by(ID,SABLE) %>% 
   mutate(
     GROUP = case_when(
-    ID %in% c(3706, 3707, 3709, 3711, 3713, 3717, 3716, 3719, 3718, 3726) ~ "ad lib",
-    ID %in% c(3708, 3714, 3720, 3721, 3710, 3722, 3723, 3724, 3725, 3727, 3728, 3729) ~ "restricted"
-  ),
-  DRUG = case_when(
-    ID %in% c(3706, 3707, 3709, 3711, 3713, 3714, 3720, 3724, 3725, 3727, 3728) ~ "vehicle",
-    ID %in% c(3708, 3710, 3716, 3717, 3718, 3719, 3721, 3722, 3723, 3726, 3729) ~ "RTIOXA_47"
-  ))
+      ID %in% c(7860, 7862, 7864, 7867, 7868, 7869, 7870, 7871, 7873, 7875, 7876, 7879, 7880, 7881,
+                7882, 7883) ~ "ad lib",
+      ID %in% c(7861, 7863, 7865, 7866, 7872, 7874, 7877, 7878) ~ "restricted"
+    ),
+    DRUG = case_when(
+      ID %in% c(7861, 7863, 7864, 7878, 7867, 7872, 7875, 7876, 7869, 7870, 7871, 7868, 7880, 7881, 7882, 7883) ~ "vehicle",
+      ID %in% c(7862, 7865, 7873, 7874, 7877, 7866, 7879, 7860) ~ "RTIOXA_47"
+    ))
   
+
 
 sable_min_plot_tee <- sable_TEE_data %>% 
   mutate(SABLE = factor(SABLE, 
                         levels = c("baseline", "peak obesity", "BW loss", 
                                    "BW maintenance", "BW regain"))) %>% 
-  filter(!(ID %in% c(3709, 3717, 3718, 3723, 3725))) #cage 5 in at least one SABLE stage
+  filter(!(ID %in% c(7866, 7874, 7877, 7879, 7864, 7881))) #cage 5 in at least one SABLE stage
 
 
 #format plot
@@ -112,7 +107,7 @@ plot <- sable_min_plot_tee %>%
   # mean ± SD ribbon
   stat_summary(
     fun.data = mean_sdl, fun.args = list(mult = 1), 
-    geom = "ribbon", aes(group = DRUG, fill = DRUG), 
+    geom = "ribbon", aes(group = GROUP, fill = GROUP), 
     alpha = 0.2, color = NA
   ) +
   
@@ -124,7 +119,7 @@ plot <- sable_min_plot_tee %>%
   
   theme_minimal() +
   labs(y = "24 h TEE", color = "Drug", fill = "Drug") +
-  facet_wrap(~GROUP) +
+  facet_wrap(~GROUP*SEX) +
   theme(
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
