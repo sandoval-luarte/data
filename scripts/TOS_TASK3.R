e58086032f35cfa5d2ab717b92f78a6f287f95a6
nicolasluarte <64244503+nicolasluarte@users.noreply.github.com>
2025-10-12 22:14:05 -0300

    updates

--- scripts/TOS_TASK3.R
+++ scripts/TOS_TASK3.R
@@ -3,22 +3,22 @@
 # libraries####
 library(dplyr)
 library(readr)
+setwd(this.path::here())
 
 final_df <- read_csv("../data/final_df_task2.csv")
 
 scaled_empirical <- final_df %>%
-  group_by(SEX, STRAIN, DIET_FORMULA) %>%
-  mutate(
-    y1_z = scale(y1),
-    y2_z = scale(y2),
-    y3_z = scale(y3),
-    y4_z = scale(y4)
-  ) %>%
-  ungroup()
+    group_by(SEX, STRAIN, DIET_FORMULA) %>%
+    mutate(
+        y1_z = scale(y1),
+        y2_z = scale(y2),
+        y3_z = scale(y3),
+        y4_z = scale(y4)
+    ) %>%
+    ungroup()
 
 scaled_empirical <- scaled_empirical %>%
-  mutate(across(ends_with("_z"), as.numeric)) #again we have some variables that are matrix
+    mutate(across(ends_with("_z"), as.numeric)) # again we have some variables that are matrix
 
 
 write_csv(scaled_empirical, "../data/scaled_empirical_task3.csv")
-
--- scripts/TOS_TASK4.R
+++ scripts/TOS_TASK4.R
@@ -1,36 +1,44 @@
-#TASK 4: To create the median split setup based on these z-scores
+# TASK 4: To create the median split setup based on these z-scores
 
 # libraries ----
 library(dplyr)
 library(readr)
 library(ggplot2)
+setwd(this.path::here())
 
-scaled_empirical <- read_csv("../data/scaled_empirical_task3.csv"). #All variables are on the same scale (mean = 0, SD = 1)
+scaled_empirical <- read_csv("../data/scaled_empirical_task3.csv") # All variables are on the same scale (mean = 0, SD = 1)
 
 # median split within groups ----
-median_split_df <- scaled_empirical %>% #the goal is to turn a continuous variable into a categorical one for classification
-  group_by(SEX, STRAIN, DIET_FORMULA) %>% #median is calculated within each combination of SEX, STRAIN, and DIET_FORMULA
-  mutate(
-    y1_group_median = median(y1_z, na.rm = TRUE),
-    y2_group_median = median(y2_z, na.rm = TRUE),
-    y3_group_median = median(y3_z, na.rm = TRUE),
-    y4_group_median = median(y4_z, na.rm = TRUE),
-    y1_split = if_else(y1_z >= y1_group_median, "High", "Low"), #each variable is transformed into a binary categorical variable (y1_split), labeled “High” or “Low”. Do we want that? ask LL
-    y2_split = if_else(y2_z >= y2_group_median, "High", "Low"),
-    y3_split = if_else(y3_z >= y3_group_median, "High", "Low"),
-    y4_split = if_else(y4_z >= y4_group_median, "High", "Low")
-  ) %>%
-  ungroup()
+median_split_df <- scaled_empirical %>% # the goal is to turn a continuous variable into a categorical one for classification
+    mutate(
+        y1_group_median = median(y1_z, na.rm = TRUE),
+        y2_group_median = median(y2_z, na.rm = TRUE),
+        y3_group_median = median(y3_z, na.rm = TRUE),
+        y4_group_median = median(y4_z, na.rm = TRUE),
+        y1_split = if_else(y1_z >= y1_group_median, "high_bw_delta", "low_bw_delta"), # each variable is transformed into a binary categorical variable (y1_split), labeled “High” or “Low”. Do we want that? ask LL
+        y2_split = if_else(y2_z >= y2_group_median, "high_adi_delta", "low_adi_delta"),
+        y3_split = if_else(y3_z >= y3_group_median, "high_bw_roc", "low_bw_roc"),
+        y4_split = if_else(y4_z >= y4_group_median, "high_adi_roc", "low_adi_roc")
+    ) %>%
+    ungroup() %>%
+    mutate(
+        median_split_combo = paste(y1_split, y2_split, y3_split, y4_split, sep = "-")
+    )
 
-table(median_split_df$y1_split, median_split_df$SEX)
-summary(median_split_df$y1_z)
+length(unique(median_split_df$median_split_combo))
 
-##plot ----
+table(median_split_df$median_split_combo)
 
-ggplot(median_split_df, aes(x = y1_z, fill = y1_split)) + #y1_z is an example
-  geom_histogram(bins = 20, alpha = 0.7, position = "identity") +
-  facet_wrap(~ SEX + STRAIN + DIET_FORMULA) +
-  theme_minimal()
+table(median_split_df$y1_split)
+table(median_split_df$y2_split)
+table(median_split_df$y3_split)
+table(median_split_df$y4_split)
+
+## plot ----
+
+ggplot(median_split_df, aes(x = y1_z, fill = y1_split)) + # y1_z is an example
+    geom_histogram(bins = 20, alpha = 0.7, position = "identity") +
+    facet_wrap(~ SEX + STRAIN + DIET_FORMULA) +
+    theme_minimal()
 
 write_csv(median_split_df, "../data/median_split_task4.csv")
-
