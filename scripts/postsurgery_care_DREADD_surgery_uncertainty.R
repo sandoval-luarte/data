1ecc6d0a5906dd699e004bec5d2ce025f6ff2811
carosandovalcab <clsandov@umn.edu>
2026-02-09 15:30:24 -0600

    cohort 17

--- scripts/postsurgery_care_DREADD_surgery_uncertainty.R
+++ scripts/postsurgery_care_DREADD_surgery_uncertainty.R
@@ -14,15 +14,30 @@ library(tidyr)
 BW_data <-read_csv("~/Documents/GitHub/data/data/BW.csv") #data import
 
 BW_data <- BW_data %>% 
-  filter(COHORT ==17) %>% 
+  filter(COHORT %in% c(11, 17)) %>% 
+  arrange(DATE) %>% 
   mutate(GROUP = case_when(
-    ID %in% c(519,524) ~ "CONTROL_NO_UNCERT",
-    ID %in% c(520,523) ~ "INH_DREADD_UNCERT",
-    ID %in% c(522,525) ~ "CONTROL_UNCERT",
-    )) 
+    ID %in% c(305, 306, 307, 314, 316, 319, 322, 519,524) ~ "CONTROL_NO_UNCERT",
+    ID %in% c(297, 313, 318, 320,520,523) ~ "INH_DREADD_UNCERT",
+    ID %in% c(321, 323, 325, 522,525) ~ "CONTROL_UNCERT",
+    )) %>% 
+  group_by(ID) %>% 
+  mutate(
+    surgery_date = min(
+      DATE[COMMENTS %in% c(
+        "BILATERAL_CONTROL_DREADD_INJECTION_SURGERY",
+        "BILATERAL_INH_DREADD_INJECTION_SURGERY"
+      )],
+      na.rm = TRUE
+    )
+  ) %>% 
+  filter(DATE >= surgery_date) %>% 
+  drop_na(GROUP) %>% 
+  filter(!(COHORT == 11 & DATE >= as.Date("2025-05-09"))) %>%
+  mutate(day_rel = as.integer(as.Date(DATE) - as.Date(first(DATE)))) 
 
 #plot####
-plot <- ggplot(BW_data, aes(x = DATE, y = BW)) +
+plot <- ggplot(BW_data, aes(x = day_rel, y = BW)) +
   geom_point(alpha = 0.7) +
   geom_line(aes(group = ID), alpha = 0.7) +
   theme_minimal() +
